4d4d098797f4211b52a0c10553b521c0e6c7e3f1
wikanadnyana <wikanadnyana44@gmail.com>
2023-06-24 11:43:15 +0800

    [FIX] detail button action

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -11,6 +11,15 @@ import {
     Td,
     TableCaption,
     TableContainer,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+    useDisclosure,
 } from "@chakra-ui/react";
 import { PieChart } from "../components/PieChart";
 
@@ -28,7 +37,57 @@ export const Create = () => {
     //   };
 
     // const [data, setData] = useState(generateData(null, null));
+    const teamName = (teamCode) => {
+        if (teamCode == 0) {
+            return "Arsenal";
+        } else if (teamCode == 1) {
+            return "Aston Villa";
+        } else if (teamCode == 2) {
+            return "Bournemouth";
+        } else if (teamCode == 3) {
+            return "Brentford";
+        } else if (teamCode == 4) {
+            return "Brighton";
+        } else if (teamCode == 5) {
+            return "Chelsea";
+        } else if (teamCode == 6) {
+            return "Crystal Palace";
+        } else if (teamCode == 7) {
+            return "Everton";
+        } else if (teamCode == 8) {
+            return "Fulham";
+        } else if (teamCode == 9) {
+            return "Leeds United";
+        } else if (teamCode == 10) {
+            return "Leicester City";
+        } else if (teamCode == 11) {
+            return "Liverpool";
+        } else if (teamCode == 12) {
+            return "Manchester City	";
+        } else if (teamCode == 13) {
+            return "Manchester United";
+        } else if (teamCode == 14) {
+            return "Newcastle";
+        } else if (teamCode == 15) {
+            return "Nottingham Forest";
+        } else if (teamCode == 16) {
+            return "Southampton	";
+        } else if (teamCode == 17) {
+            return "Tottenham";
+        } else if (teamCode == 18) {
+            return "West Ham";
+        } else if (teamCode == 19) {
+            return "Wolves";
+        }
+    };
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
     const [teamData, setTeamData] = useState([]);
+    const [detailData, setDetailData] = useState({
+        id: " ",
+        home_prediction: " ",
+        away_prediction: " ",
+    });
 
     useEffect(() => {
         AOS.init();
@@ -56,6 +115,19 @@ export const Create = () => {
         };
     }, []);
 
+    const getDetail = (id) => {
+        fetch(`http://localhost:5000/api/predictions/${id}`) // Menggunakan URL dengan ID yang diberikan
+            .then((response) => response.json())
+            .then((data) => {
+                setDetailData(data);
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+
+        onOpen();
+    };
+
     return (
         <div data-aos="fade-up">
             <TableContainer paddingTop="30px">
@@ -67,14 +139,18 @@ export const Create = () => {
                             <Th textAlign="center">Date</Th>
                             <Th textAlign="center">Group</Th>
                             <Th textAlign="center">Result</Th>
-                            {/* <Th textAlign="center">Action</Th> */}
+                            <Th textAlign="center">Details</Th>
                         </Tr>
                     </Thead>
                     <Tbody>
-                        {teamData.map((team, index) => (
-                            <Tr key={index}>
-                                <Td textAlign="center">{team.home_code}</Td>
-                                <Td textAlign="center">{team.opp_code}</Td>
+                        {teamData.map((team) => (
+                            <Tr key={team.teamId}>
+                                <Td textAlign="center">
+                                    {teamName(team.home_code)}
+                                </Td>
+                                <Td textAlign="center">
+                                    {teamName(team.opp_code)}
+                                </Td>
                                 <Td textAlign="center">{team.match_date}</Td>
                                 <Td textAlign="center">{team.group}</Td>
                                 <Td>
@@ -95,7 +171,44 @@ export const Create = () => {
                                         outerRadius={50}
                                     />
                                 </Td>
-                                <Td textAlign="center"></Td>
+                                <Td textAlign="center">
+                                    <>
+                                        <Button
+                                            onClick={() => getDetail(team.id)}
+                                        >
+                                            Detail
+                                        </Button>
+
+                                        <Modal
+                                            isOpen={isOpen}
+                                            onClose={onClose}
+                                        >
+                                            <ModalOverlay />
+                                            <ModalContent>
+                                                <ModalHeader>
+                                                    Detail Prediksi :
+                                                </ModalHeader>
+                                                <ModalCloseButton />
+                                                <ModalBody>
+                                                    Home Prediction :{" "}
+                                                    {detailData.home_prediction}{" "}
+                                                    Away Prediction :{" "}
+                                                    {detailData.away_prediction}
+                                                </ModalBody>
+
+                                                <ModalFooter>
+                                                    <Button
+                                                        colorScheme="blue"
+                                                        mr={3}
+                                                        onClick={onClose}
+                                                    >
+                                                        Close
+                                                    </Button>
+                                                </ModalFooter>
+                                            </ModalContent>
+                                        </Modal>
+                                    </>
+                                </Td>
                             </Tr>
                         ))}
                     </Tbody>
